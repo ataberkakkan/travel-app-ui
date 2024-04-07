bc02c0da29e7ec21d676cd13013543aa74782843
ataberkakkan <akkanataberk@gmail.com>
2024-04-07 22:42:28 +0300

    Group Listings Finished

--- components/GroupListings.tsx
+++ components/GroupListings.tsx
@@ -1,5 +1,6 @@
 import Colors from "@/constants/Colors";
 import { GroupListing } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
 import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 
 type GroupListingsProps = {
@@ -17,6 +18,14 @@ const GroupListings = ({ listings }: GroupListingsProps) => {
         renderItem={({ item }) => (
           <View style={styles.item}>
             <Image source={{ uri: item.image }} style={styles.image} />
+            <View>
+              <Text style={styles.itemTxt}>{item.name}</Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Ionicons name="star" size={20} color={Colors.primaryColor} />
+                <Text style={styles.itemRating}>{item.rating}</Text>
+                <Text style={styles.itemReviews}>({item.reviews})</Text>
+              </View>
+            </View>
           </View>
         )}
       />
@@ -33,14 +42,33 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   item: {
+    flexDirection: "row",
     backgroundColor: Colors.white,
     padding: 10,
     borderRadius: 10,
     marginRight: 20,
+    alignItems: "center",
   },
   image: {
     width: 80,
     height: 100,
     borderRadius: 10,
+    marginRight: 10,
+  },
+  itemTxt: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 8,
+  },
+  itemRating: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: Colors.black,
+    marginLeft: 5,
+  },
+  itemReviews: {
+    fontSize: 14,
+    color: "#999",
   },
 });
